7919f8f4b77283a8de2d6eb382aa125cbf2ab352
Pariksheet Nanda <p.nanda@andor.com>
2011-07-27 20:09:03 -0400

    removed unnecessary nesting in serial_check

--- laser.py
+++ laser.py
@@ -12,6 +12,7 @@ Melles Griot: 560
 
 from serial import Serial
 from time import sleep
+from string import rjust
 
 class Laser:
   '''Serial laser device communication'''
@@ -42,57 +43,96 @@ class Laser:
       self.ser.close() # since an exception leaves the COM port open
       raise
   
-  def serial_check(self, command_list, expected_output=None):
+  def serial_check(self, command_list, expected_output_list=None, verbose=True):
     '''Process multiple serial commands compare against expected output.
     
     Arguments:
-    command_list    -- list or tuple of queries to serial device
-                       e.g. ['>=0', 'L=1']
+    command_list --
+      list or tuple of queries to serial device
+      e.g. ['>=0', 'L=1']
     
     Keyword Arguments:
-    expected_output -- list or tuple of response from serial device
-                       e.g. [None, 'OK']
+    expected_output_list --
+      list or tuple of response from serial device
+      e.g. [None, 'OK']
     
-    The command will still go through if no expected output is provided, or if
-    the output for a single command is `None`.  This is to account for 'dumb'
-    devices which do not respond, but otherwise it's bad practise so a warning
-    is printed.
+    A single command will still go through if no expected output is provided.
+    This is to account for 'dumb' devices which do not respond, and for testing
+    purposes, but otherwise it's bad practise so a warning is printed.  If one
+    expects no output for a command, use `None`.
     '''
-    if isinstance(command_list,tuple) or isinstance(command_list,list):
-      for command in command_list:
-        #print 'DEBUG: About to write serial command'
-        self.ser.write(command + '\r\n')
-        sleep(0.01) # wait 10ms
-        output = self.ser.readlines() # takes seconds to read all lines
+    output_list = []
+    
+    if not (isinstance(command_list,tuple) or isinstance(command_list,list)):
+      print '(EE) serial_check(): Expected command_list as list or tuple'
+      return None
+    
+    while len(expected_output_list) < len(command_list):
+      # Add `None` till the length matches
+      expected_output_list.append(None)
+    
+    for i in range(0,len(command_list)):
+      command = command_list[i]
+      expected_output = expected_output_list[i]
+      #print 'DEBUG: About to write serial command'
+      self.ser.write(command + '\r\n')
+      sleep(0.01) # wait 10ms
+      output = self.ser.readlines() # takes seconds to read all lines
+      
+      if verbose:
+        print rjust(command,20), ":", output,
+      
+      if expected_output is None:
+        # One should always specify the output but don't make a big deal of it
+        print '(WW) serial_check(): Output of', command, ':', output,\
+              'but no expected output was stated'
+        return output_list.append(output)
+      
+      if output is None:
+        # Laser does not give output.
+        # Assume it is 'dumb', so function terminates ok
+        output_list.append(None)
+        continue
         
-        if expected_output is None:
-          # One should always specify the output but don't make a big deal of it
-          print '(WW) serial_check(): Output of', command, ':', output
-          print '     But no expected output was stated'
-          return True
+      if not isinstance(output,list): # readlines always returns a list
+        # Output should always be a list
+        print '(EE) serial_check(): Output of', command, ':', output,\
+              'but cannot parse since it is not a list'
+        output_list.append(output)
+      
+      output_found = 0
+      iterate_expected = 1
+      expected_output_item = expected_output
+      
+      if isinstance(expected_output,list) or\
+         isinstance(expected_output,tuple):
+        iterate_expected = len(expected_output)
+        print "Output list length =", iterate_expected
+        expected_output_item = expected_output[0]
+      
+      while iterate_expected > 0:
         
-        if output is not None:
-          if isinstance(output,list): # readlines always returns a list
-            for string in output:
-              string = string.strip(self.JUNK_CHARACTERS)
-              if string in expected_output:
-                return string
-            
-            print '(EE) serial_check(): Expected', expected_output,\
-                  'but got', output
-            return False
-          else:
-            # Output should always be a list
-            print '(EE) serial_check(): Output of', command, ':', output
-            print '     But cannot parse since it is not a list'
+        for string in output:
+          stripped_output = string.strip(self.JUNK_CHARACTERS)
+          if stripped_output == expected_output_item:
+            output_list.append(stripped_output)
+            output_found = 1
+            break
+            if verbose:
+              print '-OK'
+          iterate_expected -= 1
+          print "Iterate expected =", iterate_expected
+          if iterate_expected > 0:
+            expected_output_item = expected_output[iterate_expected]
         
-        else:
-          # Laser does not give output.
-          # Assume it is 'dumb', so function terminates ok
-          return True
-    else:
-      print 'ERR serial_check(): Expected command_list as list or tuple'
-      return False
+        if output_found:
+          break
+      
+      if not output_found:
+        print '-Expected output not found'
+        print '(WW) serial_check(): Expected', expected_output_item,\
+              'but got', output
+        output_list.append(False)
   
   def run(self, loop=False):
     '''Loop the laser state machine until it reaches '''
@@ -106,10 +146,12 @@ class Laser:
     }
     
     # Initial state
-    self.serial_check(self.INIT, ['>=0'])
+    self.serial_check(self.INIT, ['>=0', 'L=1'])
     machine_state = 's0'
     state_history = []
-    machine_input = self.serial_check(self.CHECK_STATUS, str(range(1,7)))
+    machine_input = self.serial_check(self.CHECK_STATUS,
+                                      self.list_to_str(range(1,7)))
+    print 'Machine input:', machine_input
     
     i = 100 # FIXME: timeout
     
@@ -136,6 +178,7 @@ class Laser:
         
       if next_command is not None:
         machine_input = self.serial_check(self.CHECK_STATUS, str(range(1,7)))
+        print 'Machine input:', machine_input
       
       i -= 1
     
@@ -180,6 +223,13 @@ class Laser:
       for input in inputs:
         expanded[(state, (input,))] = value
     return expanded
+  
+  def list_to_str(self, number_list):
+    list_of_strings = []
+    for number in number_list:
+      list_of_strings.append(str(number))
+    return tuple(list_of_strings)
+
 
 class Sapphire(Laser):
   '''Laser control of Coherent Sapphire'''
@@ -196,22 +246,23 @@ class Sapphire(Laser):
     # State transition table, grouping state inputs in 'shorthand'
     TRANSITION_SHORTHAND = {
       ('s0', ('6',)): ('s1', 1, self.INIT),
-      ('s0', tuple(str(range(1,5)))): ('s2', 1, self.ON),
+      ('s0', self.list_to_str(range(1,5))): ('s2', 1, self.ON),
       ('s0', ('5',)): ('s3', 1, None),
-      ('s1', tuple(str(range(1,5)))): ('s2', 1, self.ON),
+      ('s1', self.list_to_str(range(1,5))): ('s2', 1, self.ON),
       ('s1', ('5',)): ('s3', 1, None),
       ('s1', ('6',)): ('s4', 0, None),
       ('s2', ('5',)): ('s3', 1, None),
-      ('s2', tuple(str(range(1,5)))): ('s2', 1, self.ON),
+      ('s2', self.list_to_str(range(1,5))): ('s2', 1, self.ON),
       ('s2', ('6',)): ('s2', 0, None),
       ('s3', ('5',)): ('s3', 1, None),
       ('s3', ('6',)): ('s3', 0, None),
-      ('s3', tuple(str(range(1,5)))): ('s3', 0, None),
+      ('s3', self.list_to_str(range(1,5))): ('s3', 0, None),
     }    
     self.TRANSITION_MATRIX = self.transition_expand(TRANSITION_SHORTHAND)
     
     # Boilerplate for running the laser
     Laser.__init__(self, port)
+
 
 class Cobolt4(Laser):
   '''Laser control of Cobolt Generation 4'''
